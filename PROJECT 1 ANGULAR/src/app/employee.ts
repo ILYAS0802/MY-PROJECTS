5e8cb0ee29576ea1b9ce12d1fe6bb07d356a1e66
ILYAS0802 <140304570+ILYAS0802@users.noreply.github.com>
2023-07-23 03:21:26 -0400

    Add files via upload

---
+++ PROJECT 1 ANGULAR/src/app/employee.ts
@@ -0,0 +1,9 @@
+export class Employee {
+    constructor(public id?: any, public firstName?: String, public lastName?: String, public emailId?: String){
+        this.id = id;
+        this.firstName= firstName;
+        this.lastName= lastName;
+        this.emailId=emailId;
+
+}
+}
